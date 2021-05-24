6b49512a4ebd608a6e3c6d48c1ee4e0955d40ce6
lot8229 <lot8229@kakao.com>
2021-05-24 10:41:42 +0900

    datatype done

--- Datatype.js
+++ Datatype.js
@@ -27,3 +27,23 @@ function changebool() {
 var sym = Symbol("javascript");
 var symObj = Object(sym);// 심볼은 유일하고 변경할 수 없는 타입으로, 객체의 프로퍼티를 위한 식별자로 사용할 수 있습니다.
 
+/*-----typeof 연산자------*/
+function changetype() {
+  document.getElementById("num").innerHTML = (typeof 10);
+  document.getElementById("string").innerHTML = (typeof "Strings");
+  document.getElementById("bool").innerHTML = (typeof true);
+  document.getElementById("undefine").innerHTML = (typeof undefined);
+  document.getElementById("obj").innerHTML = (typeof null);
+}
+
+function same() {
+  document.getElementById("gogo").innerHTML=(null == undefined)
+  document.getElementById("dif").innerHTML=(null === undefined)
+
+}
+
+/*-----object 연산자------*/
+function objtest() {
+  var dog = { name : "happy",age : 3};
+  document.getElementById("result2").innerHTML ="강아지의 이름은" + dog.name + "이고 , 나이는" + dog.age+"살 입니다.";
+}
