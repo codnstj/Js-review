87145311c5a603cdaa4e8d5f88d25eaeb0464be0
lot8229 <lot8229@kakao.com>
2021-05-30 21:45:51 +0900

    opentutorials comparison

--- js/boolean.js
+++ js/boolean.js
@@ -2,3 +2,5 @@ console.log(true);
 console.log(false);
 
 // 불린 타입의 표현형
+
+
---
+++ js/comparison.js
@@ -0,0 +1,14 @@
+//comparison 타입(비교 연산자)
+console.log(1+1);
+//2
+
+console.log(1==1);
+//true
+console.log(1==2);
+//false
+
+console.log(1===1);
+// perfectly correct
+
+var name = 1;
+//대입 연산
