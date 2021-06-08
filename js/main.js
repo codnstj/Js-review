640981c59addb6a0255070ef4f1e9faff3646b57
lot8229 <lot8229@kakao.com>
2021-06-08 20:45:14 +0900

    adding binary

--- js/main.js
+++ js/main.js
@@ -1,43 +1,71 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require('http'); //http 라이브러리 
+var fs = require('fs');    // 파일 시스템 라이브러리
+var url = require('url'); // url 라이브러리 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
+    var _url = request.url; //
     var queryData = url.parse(_url,true).query;
-    console.log(queryData.id);
-    console.log(_url);
-    var title = queryData.id;
-
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`../Data/${queryData.id}`,'utf8',function(err,description){
+    var pathname = url.parse(_url,true).pathname;
+    
+    if(pathname ==='/'){
+      if(queryData.id === undefined){
+        fs.readFile(`../Data/${queryData.id}`,'utf8',function(err,description){
+        var title = 'welcome';
+        var description = 'Hello, Node.JS';
         var Temeplete = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href='/?id=HTML'>HTML</a></li>
-        <li><a href='/?id=CSS'>CSS</a></li>
-        <li><a href='/?id=JavaScripts'>JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href='/?id=HTML'>HTML</a></li>
+              <li><a href='/?id=CSS'>CSS</a></li>
+              <li><a href='/?id=JavaScripts'>JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(Temeplete);
+        })
+      }else{
 
-    response.end(Temeplete);
-    })
+        fs.readFile(`../Data/${queryData.id}`,'utf8',function(err,description){
+          var title = queryData.id;
+          var Temeplete = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href='/?id=HTML'>HTML</a></li>
+              <li><a href='/?id=CSS'>CSS</a></li>
+              <li><a href='/?id=JavaScripts'>JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(Temeplete);
+        })}
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+
+   
+
 
     
 
