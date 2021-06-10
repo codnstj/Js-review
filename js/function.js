ab1f57f94e62e5fe9747476b71d3e9b30f739aca
lot8229 <lot8229@kakao.com>
2021-06-10 13:32:58 +0900

    Function'

---
+++ js/function.js
@@ -0,0 +1,14 @@
+console.log('a');
+console.log('b');
+console.log('c');
+
+console.log('---');
+
+f1234();
+
+
+function f1234(){
+  console.log('a');
+  console.log('b');
+  console.log('c');
+}
---
+++ js/function2.js
@@ -0,0 +1,13 @@
+console.log(Math.round(1.6)); //2 반올림
+
+console.log(Math.round(1.4)); //1 반올림
+
+//입력값을 가진다.
+
+function sum(first,second) {  //Parameter
+ return first+second;
+}
+
+console.log(sum(2,4)); // 입력 값 하나하나 를 Argument 라고 한다.
+
+
