9d0a92a4cc9170951ce6d34e41884ba32e3fcf7c
lot8229 <lot8229@kakao.com>
2021-06-16 22:53:17 +0900

    editing all errors

--- node/index.js
+++ node/index.js
@@ -12,7 +12,8 @@ app.use(cookieParser());
 //application/json
 app.use(bodyParser.json()) 
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { json } = require('body-parser');
 mongoose.connect(config.mongoURI,{
   useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true,useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected'))
@@ -39,29 +40,32 @@ app.post('/register',(req,res) => {
 
 
 app.post('/login',(req,res) => {
-   
+   console.log(JSON.stringify(req.body))
   //요청된 이메일 을 데이터베이스에서 있는지 찾는다.
   User.findOne({email:req.body.email},(err,user)=> {
+    console.log(JSON.stringify(user))
     if(!user){
       return res.json({
         loginSuccess:false,
         message:"제공된 이메일에 해당하는 유저가 없습니다."
       })
-  }
+    }
     //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번혼지 확인
 
     user.comparePassword(req.body.password ,(err,isMatch) => {
-        if(!isMatch)
-          return res.json({loginSuccess : false,message : "비밀번호가 틀렸습니다."})
-
+      if(!isMatch){
+        console.log(1);
+        return res.json({loginSuccess : false,message : "비밀번호가 틀렸습니다."})
+        
+      }
             //비밀번호까지  맞다면 토큰을 생성하기. (JSONWEBTOKEN 설치)
       user.generateToken((err,user)=>{
-          if(err) return res.status(400).send(err);
-          else{
-            res.cookie("x_auth",user.token)
+        if(err) return res.status(400).send(err);
+        else{
+          res.cookie("x_auth",user.token) //쿠키에 저장
             .status(200)
             .json({loginSuccess:true,userId:user._id})
-          }
+        }
           //토큰을 저장한다. 어디에 ? 쿠키,로컬스토리지,
           
       })  
--- node/models/User.js
+++ node/models/User.js
@@ -57,7 +57,8 @@ userSchema.pre('save',function(next){
 
     //plainPassword 1234567  == 암호화된 비밀번호 : ~~
       bcrypt.compare(plainPassword,this.password, function(err,isMatch) {
-        if(err) return cb(err),
+        console.log(isMatch);
+        if(err) return cb(err);
         cb(null,isMatch)
       })
   }
