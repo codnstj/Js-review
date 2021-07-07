3dc7da67c8def6c8ca58d2f02f1564e2df961c39
lot8229 <lot8229@kakao.com>
2021-07-07 15:01:58 +0900

    sequalize index.js

--- Node JS/회원가입&로그인 구현(mysql)/HelloExpress/routes/index.js
+++ Node JS/회원가입&로그인 구현(mysql)/HelloExpress/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+//connection 연결
+let client = mysql.createConnection({
+  user:"chaewoon",
+  password:"password",
+  database:"mysqltest"
+})
+// client.connect(err=>{
+//   if(err)throw err;
+//   console.log("Mysql Connected...")
+// })
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hello world');
 });
 
 module.exports = router;
